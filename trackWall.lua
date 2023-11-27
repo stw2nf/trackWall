e1f1b1a96c9ec166383903e8c81c9d92a87811d5
stw2nf <stw2nf@mail.umkc.edu>
2023-11-27 10:52:59 -0600

    Add custom parameter table: TRACK_P -- Proportional Gain for Controller TRACK_DIST -- Distance to hold from wall (ft) TRACK_SP -- Maximum throttle setting for constant speed TRACK_STR -- Steering limit TRACK_D -- Derivative Gain for Controller

--- trackWall.lua
+++ trackWall.lua
@@ -10,22 +10,30 @@ local errorOld = 0
 local rover_guided_mode_num = 15
 local steeringOut
 
-local pGain = param:get('SCR_USER1') -- Proportional Gain for Controller
-local offsetDist = param:get('SCR_USER2') -- Distance to hold from wall (ft)
-local cruiseMax = param:get('SCR_USER3') -- Maximum throttle setting for constant speed
-local limSteer = param:get('SCR_USER4') -- Steering limit
-local dGain = param:get('SCR_USER5') -- Derivative Gain for Controller
+local PARAM_TABLE_KEY = 73
+
+assert(param:add_table(PARAM_TABLE_KEY, "TRACK_", 5), 'could not add param table') -- Track Wall parameter table 
+
+-- create two parameters. The param indexes (2nd argument) must
+-- be between 1 and 63. All added parameters are floats, with the given
+-- default value (4th argument).
+assert(param:add_param(PARAM_TABLE_KEY, 1, 'P', 0.2), 'could not add TRACK_P') -- Proportional Gain for Controller
+assert(param:add_param(PARAM_TABLE_KEY, 2, 'DIST', 1), 'could not add TRACK_DIST') -- Distance to hold from wall (ft)
+assert(param:add_param(PARAM_TABLE_KEY, 3, 'SP', 1), 'could not add TRACK_SP') -- Maximum throttle setting for constant speed
+assert(param:add_param(PARAM_TABLE_KEY, 4, 'STR',0.25), 'could not add TRACK_STR') -- Steering limit
+assert(param:add_param(PARAM_TABLE_KEY, 5, 'D', 0.1), 'could not add TRACK_D') -- Derivative Gain for Controller
+
+local pGain = param:get('TRACK_P') -- Proportional Gain for Controller
+local offsetDist = param:get('TRACK_DIST') -- Distance to hold from wall (ft)
+local cruiseMax = param:get('TRACK_SP') -- Maximum throttle setting for constant speed
+local limSteer = param:get('TRACK_STR') -- Steering limit
+local dGain = param:get('TRACK_D') -- Derivative Gain for Controller
 local dError = 0 -- Derivative of Error
 local alignThresh = 1 -- cm threshold
 
 local speedRC = rc:find_channel_for_option(300)
 local directionRC = rc:find_channel_for_option(301)
 local cruiseSpeed
-
-local start_button_number = 1 -- Start Button
-local trigger_button_state = false
-local start_button_new_state = button:get_button_state(start_button_number)
-local start_button_old_state = start_button_new_state
 
 local rad2deg = 180/math.pi
 local updateRate = 100
@@ -36,7 +44,7 @@ local directionRC_posOld = 0
 local startFlag
 
 local pMult = 4
-local rng1_offset = -5 -- offset in cm
+local rng1_offset = 0 -- offset in cm
 local rng2_offset = 0 -- offset in cm
 vehicle:set_mode(rover_guided_mode_num)
 
@@ -59,7 +67,7 @@ function alignVehicle()
   else
     vehicle:set_steering_and_throttle(0.0, 0.0)
     gcs:send_text(6, "Vehicle is Aligned")
-    param:set('SCR_USER2', actualDist)
+    param:set('TRACK_DIST', actualDist)
     offsetDist = actualDist
     gcs:send_text(6, "Tracking Wall at: "..tostring(offsetDist))
     return update, 5000
@@ -77,12 +85,12 @@ function limitSteer(input, limit)
 end
 
 function updateParams()
-  pGain = param:get('SCR_USER1')
-  cruiseMax = param:get('SCR_USER3')
-  limSteer = param:get('SCR_USER4')
-  dGain = param:get('SCR_USER5')
+  pGain = param:get('TRACK_P')
+  cruiseMax = param:get('TRACK_SP')
+  limSteer = param:get('TRACK_STR')
+  dGain = param:get('TRACK_D')
 
-  local speedRC_pos = speedRC:get_aux_switch_pos()
+  local speedRC_pos = speedRC:get_aux_switch_pos() -- Set speed parameter based on Aux Switch Position
   if speedRC_pos == 0 then
     cruiseSpeed = cruiseMax
   elseif speedRC_pos == 1 then
@@ -90,20 +98,20 @@ function updateParams()
   else
     cruiseSpeed = cruiseMax/3
   end
+
   directionRC_posOld = directionRC_pos
   directionRC_pos = directionRC:get_aux_switch_pos()
   --gcs:send_text(6, "Switch Postition: "..tostring(directionRC_pos))
   if directionRC_pos == 2 and directionRC_posOld ~= directionRC_pos then
-    param:set('SCR_USER1', -pGain)
-    param:set('SCR_USER3', -cruiseMax)
-    param:set('SCR_USER5', -dGain)
+    param:set('TRACK_P', -pGain)
+    param:set('TRACK_SP', -cruiseMax)
+    param:set('TRACK_D', -dGain)
 
-    if param:get('SCR_USER3') > 0 then
+    if param:get('TRACK_SP') > 0 then
       gcs:send_text(6, "Forward")
     else
       gcs:send_text(6, "Reverse")
     end
-    
   end
 end
 
@@ -115,20 +123,14 @@ function update()
     startFlag = 1
     return alignVehicle, 1
   end
-
   if arming:is_armed() and vehicle:get_mode() == rover_guided_mode_num then
-    
-    
     dist1 = ((rangefinder:distance_cm_orient(7)+rng1_offset)/100)*m2ft
     dist2 = ((rangefinder:distance_cm_orient(5)+rng2_offset)/100)*m2ft
 
     actualDist = averageDist(dist1, dist2)
-
-    --gcs:send_text(6, "Measured " .. tostring(measuredDist) .. " Adjusted: " .. tostring(actualDist))
     errorOld = error
     error = offsetDist - actualDist
     dError = (error - errorOld)/(updateRate/1000)
-
     steeringOut = limitSteer(pGain*error + dGain*dError, limSteer)
 
     gcs:send_text(6, "Steering Out " .. tostring(steeringOut) .. " P: " .. tostring(pGain*error).." D: "..tostring(dGain*dError))
